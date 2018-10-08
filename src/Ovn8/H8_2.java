2efa8937e2e768179937b6a3e8fc6e4414bb428b
os1866pe-s <43028719+os1866pe-s@users.noreply.github.com>
2018-10-08 16:02:39 +0200

    update

--- src/Ovn8/H8_2.java
+++ src/Ovn8/H8_2.java
@@ -7,22 +7,49 @@ public class H8_2 {
     public static void main(String[] args){
 
         Scanner userInput = new Scanner(System.in);
-        System.out.print("Enter a number with destination(num, index): ");
-        int numToInsert = userInput.nextInt();
-        int index = userInput.nextInt();
-        int zeroIndex;
 
         int vector[] = {2, -5, 14, 21, -7, 0, 0};
+        int vecLength = vector.length;
 
-        /*
-        System.out.println(zeroIndex);
+        boolean containZero = false;
+        boolean zeroIndex[] = new boolean[vecLength];
 
-        for (int i = zeroIndex; i > (index); i--){
-            vector[i] = vector[i-1];
+        for (int i = 0; i < vecLength; i++){
+            if (vector[i] == 0){
+                containZero = true;
+                zeroIndex[i] = true;
+            }else {
+                zeroIndex[i] = false;
+            }
+        }
+        if (containZero){
+            System.out.print("Enter a number with destination(num, index): ");
+            int numToInsert = userInput.nextInt();
+            int numToInsertIndex = userInput.nextInt();
+
+            if (zeroIndex[numToInsertIndex]){
+                vector[numToInsertIndex] = numToInsert;
+            }else {
+
+
+                int j = numToInsertIndex; //3
+
+                int temp2; //-7
+                int temp1 = vector[j]; //21
+                vector[j] = numToInsert; //insert 3 into index 3
+                j++; //4
+
+                while (j < vecLength){ //4
+                    temp2 = vector[j]; // -7
+
+                    vector[j] = temp1; //insert 21 into index 4
+                    temp1 = temp2; //next number is switched to -7
+                    j++;
+                }
+            }
 
         }
-        vector[index] = numToInsert;
         System.out.println(Arrays.toString(vector));
-        */
+
     }
 }
