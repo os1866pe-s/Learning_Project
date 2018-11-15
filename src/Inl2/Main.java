2bf286c1ea6b6d66291c80bcc3e27047a3a9b0d7
os1866pe-s <43028719+os1866pe-s@users.noreply.github.com>
2018-11-15 12:27:12 +0100

    K

--- src/Inl2/Main.java
+++ src/Inl2/Main.java
@@ -4,12 +4,34 @@ public class Main {
     public static void main(String[] args) {
 
         Dialog dialog = new Dialog();
+        String menu = "Menu" +
+                "\n1: Sätt in en ny skiva " +
+                "\n2: Tag bort en artists skivor" +
+                "\n3: Tag bort en CD med skivnamnet" +
+                "\n4: Sök skivor från artistnamn/del av artistnamn" +
+                "\n5: Sök skivor från titeln/del av titeln" +
+                "\n6: Se alla skivor sorterade efter artist" +
+                "\n7: Se alla skivor sorterade efter titel";
 
-        //dialog.printString("This is some text");
-        int test = dialog.readInt("Write a int");
+        String artist;
 
-        String text = dialog.readString("Is this text");
 
-        System.out.println("You wrote " + test + " and " + text);
+        while (true){
+            int choosenMenu = dialog.readInt(menu);
+
+            switch (choosenMenu){
+                case 1:
+
+
+                case 2: ;
+
+                case 3: ;
+
+                default: ;
+            }
+
+        }
+
+
     }
 }
--- src/Inl2/Register.java
+++ src/Inl2/Register.java
@@ -1,26 +1,87 @@
 package Inl2;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Register {
 
 
     private ArrayList<CD> reg;
-    // registret skall lagras i en ArrayList
-
-
 
     public Register(){
+        reg = new ArrayList<CD>();
+    }
+
+    /**Adds a new CD with an artist and a title.*/
+    public void addCD(String artist, String title){
+        reg.add(new CD(artist,title));
 
     }
+
+    /**Removes all CD's with the artist(artist).*/
+    public void removeAllFromArtist(String artist){
+        List<CD> test = reg.stream().filter(x -> !x.getArtist().equals(artist))
+                .collect(Collectors.toList());
+
+
+
+
+    }
+
+    /**Removes all CD's with the title(title).*/
+    public void removeAllWithTitle(String title){}
+
+    /**Finds all of the CD's from the artist(artist).*/
+    public void searchByArtist(String artist){}
+
+    /**Finds the CD with the title(title).*/
+    public void searchByTitle(String title){}
+
+    /**Returns all the CD's with both the artist and title
+     * sorted by artist.*/
+    public void getSortedByArtist(){}
+
+    /**Returns all the CD's with both the artist and title
+     * sorted by title.*/
+    public void getSortedByTitle(){}
 
 
     /** Läser registret från filen med namn fileName. */
     public void readFromFile(String fileName) {
-    //...  Se ledning för filhanteringen sist i uppgiften
+
+        String line;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+
+            while ((line = br.readLine()) != null){
+
+            }
+
+            br.close();
+        } catch (IOException e) {
+            System.out.println("File was not found.");
+        }
+
     }
 
     /** Sparar registret på fil med namnet fileName. */
     public void writeToFile(String fileName) {
-    //...  Se ledning förfilhanteringen sist i uppgiften
+
+        String line;
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName)));
+
+
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("IO exception");
+        }
+
+
     }
 }
