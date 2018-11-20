2f6128dc5ed66a6ed037fef2063178bde3f78480
Ossy33 <kladekaka99@gmail.com>
2018-11-20 13:11:42 +0100

    Updated

---
+++ src/UserInterfaces/UIProgram_1/Layout.java
@@ -0,0 +1,10 @@
+package UserInterfaces.UIProgram_1;
+
+import javax.swing.*;
+
+public class Layout {
+    private JTextField SearchBar;
+    private JButton addButton;
+    private JButton Remove;
+    private JTextArea textArea1;
+}
